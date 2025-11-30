9df6669775a0a1d98bf93046b845410388d50423
Mohamed Kassim Sinan <mohamed.kassim.sinan@gmail.com>
2025-11-30 11:04:35 +0400

    Deploy App

--- expenses_website/expenses_website/settings.py
+++ expenses_website/expenses_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
+
